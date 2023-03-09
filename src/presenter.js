9dfffae24d0b13054e228bdd948a2c8836e1a785
fabi-cm <dahofcmmejia@gamil.com>
2023-03-09 09:22:52 -0400

    No se puede publicar un post sin un titulo, es obligatorio

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,25 @@
 import mostrar_post from "./mostrar_posts";
+import verificar_titulo from "./verificar_titulo";
 
 const titulo = document.querySelector("#titulo");
 const detalle = document.querySelector("#detalle");
 const post_form = document.querySelector("#post-form");
 const div_post = document.querySelector(".div_post");
+const titulo_obligatorio = document.querySelector("#titulo_obligatorio")
 const item_post = document.querySelector(".item-post");
 
 let posts = [];
 
 post_form.addEventListener("submit", (event) => {
   event.preventDefault();
-
+  if(verificar_titulo(titulo.value,titulo_obligatorio)){
+    return;
+  }
   const objPost = {
     id: Date.now(),
     titulo: titulo.value,
     detalle: detalle.value,
   }
-
   posts = [...posts, objPost];
   post_form.reset();
   mostrar_post(posts, div_post);
---
+++ src/verificar_titulo.js
@@ -0,0 +1,13 @@
+function verificar_titulo(titulo, titulo_obligatorio) {
+    let resp = false;
+    if (!titulo.trim()) {
+        titulo_obligatorio.textContent = 'Titulo Obligatorio';
+  
+        setTimeout(() => {
+            titulo_obligatorio.textContent = '';
+        }, 2000)
+        resp = true;
+    }
+    return resp;
+}
+export default verificar_titulo
